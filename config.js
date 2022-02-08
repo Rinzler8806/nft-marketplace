ae95086a76ad5e1107b36a87bed41cfc014586ba
Rinzler8806 <shane.rogers916@gmail.com>
2022-02-07 22:49:59 -0800

    project mostly complete. Ready to deploy to vercel

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
-export const nftmarketaddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+export const nftaddress = "0xC8F9A1592087C377377f41b0676BF703e424896a"
+export const nftmarketaddress = "0xE19c3623e2f8ea8638f70Cc7B94234285bf68896"
